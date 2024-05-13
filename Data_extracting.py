a1a75959efc5c626db6e34961c6c32266a46467a
ValentinaIvanic <ivanic.valentina2@gmail.com>
2024-05-13 21:33:37 +0200

    Data Extracting functions

---
+++ Data_extracting.py
@@ -0,0 +1,113 @@
+import pandas as pd
+
+def get_dataFromEurostat(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name=sheet_name, header=None, skiprows=7)
+
+    filtered_df = df[df[0].isin(['TIME', 'Croatia'])]
+
+    filtered_df = filtered_df[~filtered_df[0].isna()]
+    filtered_df = filtered_df.dropna(axis=1, how='all')
+
+    return filtered_df
+
+def get_dataFromHNB(file_path, sheet_name, header):
+    df = pd.read_excel(file_path, sheet_name=sheet_name, header=header, skiprows=1)
+
+    df.columns = df.iloc[0]
+    df = df.drop(0)
+
+    df.reset_index(drop=True, inplace=True)
+    filtered_df = df.dropna(how='all')
+
+    
+    # print(filtered_df)
+
+    return filtered_df
+
+# --------------------------------------------<< Data from Eurostat >> ----------------------------------------------------
+
+def get_deaths():
+    return get_dataFromEurostat("Data\Deaths (total) by month.xlsx", "Sheet 1")
+
+def get_births():
+    return get_dataFromEurostat("Data\Live births (total) by month.xlsx", "Sheet 1")
+
+def get_population():
+    return get_dataFromEurostat("Data\Population on 1 January by age and sex.xlsx", "Sheet 1")
+
+#-------
+
+def get_womenAgeFirstBirth():
+    return get_dataFromEurostat("Data\Fertility indicators - mean women age at first child birth.xlsx", "Sheet 1")
+
+def get_womenCompletedAgeFirstMarriage():
+    return get_dataFromEurostat("Data\First marriage rates by age and sex - females.xlsx", "Sheet 1")
+
+def get_manCompletedAgeFirstMarriage():
+    return get_dataFromEurostat("Data\First marriage rates by age and sex - males.xlsx", "Sheet 1")
+
+def get_womenAgeFirstMarriage():
+    return get_dataFromEurostat("Data\First-time marrying persons by age and sex.xlsx", "Sheet 1")
+
+def get_HICPbyMonth():
+    return get_dataFromEurostat("Data\Harmonised index of consumer prices mjesečno.xlsx", "Sheet 1")
+
+# --------------------------------------------<</ Data from Eurostat >> ----------------------------------------------------
+
+
+
+# --------------------------------------------<< Data from HNB >> ----------------------------------------------------
+
+def get_PriceIndexResidentalBuilding():
+    df = get_dataFromHNB("Data\Indeksi cijena stambenih objekata.xlsx", "HRV", 2)
+    df = df.iloc[:-4]
+    print(df)
+    return df
+
+def get_ConsumerIndexes():
+    df = get_dataFromHNB("Data\Indeksi pouzdanja, očekivanja, raspoloženja potrošača.xlsx", "HRV", 3)
+    df = df.iloc[:-1]
+    print(df)
+    return df
+
+def get_ConsumerProducerPricesIndex():
+    df = get_dataFromHNB("Data\Indeksi potrošačkih cijena i prozivođačkih cijena industrije.xlsx", "HRV", 2)
+    df = df.iloc[:-3]
+    print(df)
+    return df
+
+def get_FundamentalConsumerPriceIndexes():
+    df = get_dataFromHNB("Data\Temeljni indeksi potrošačkih cijena.xlsx", "HRV", 4)
+    df = df.iloc[:-2]
+    print(df)
+    return df
+
+def get_HICP():
+    df = get_dataFromHNB("Data\Harmonizirani indeksi potrošačkih cijena.xlsx", "HRV", 2)
+    df = df.iloc[:-1]
+    print(df)
+    return df
+
+def get_AverageSalaryByMonth():
+    df = get_dataFromHNB("Data\Prosječna mjesečna neto plaća i indeksi.xlsx", "EUR", 3)
+    df = df.iloc[:-2]
+    print(df)
+    return df
+
+# --------------------------------------------<</ Data from HNB >> ----------------------------------------------------
+
+
+# get_births()
+# get_womenAgeFirstBirth()
+# get_womenCompletedAgeFirstMarriage()
+# get_manCompletedAgeFirstMarriage()
+# get_HICPbyMonth()
+# get_births()
+# get_population()
+
+get_PriceIndexResidentalBuilding()
+get_ConsumerIndexes()
+get_ConsumerProducerPricesIndex()
+get_FundamentalConsumerPriceIndexes()
+get_HICP()
+get_AverageSalaryByMonth()
